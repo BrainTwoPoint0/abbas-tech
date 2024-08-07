877cf1b05d08901b32b1ae3beabacad52501cf82
Karim Fawaz <karimhilalfawaz@gmail.com>
2024-08-08 00:26:28 +0100

    bulk updates

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -12,12 +12,9 @@ const Hero = () => (
     <Section yPadding="py-6">
       <NavbarTwoColumns logo={<Logo xl />}>
         <li>
-          <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
-            GitHub
+          <Link href="mailto:abbas.a.kazmi@outlook.com">
+            Contact
           </Link>
-        </li>
-        <li>
-          <Link href="/">Sign in</Link>
         </li>
       </NavbarTwoColumns>
     </Section>
