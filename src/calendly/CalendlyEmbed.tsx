ed76214f909788992b570576975f02d9965ec2aa
Karim Fawaz <karimhilalfawaz@gmail.com>
2024-08-08 02:03:05 +0100

    Update CalendlyEmbed.tsx

--- src/calendly/CalendlyEmbed.tsx
+++ src/calendly/CalendlyEmbed.tsx
@@ -3,25 +3,25 @@
 import React, { useEffect } from 'react';
 
 const CalendlyEmbed = ({ url }: { url: string }) => {
-    useEffect(() => {
-        const head = document.querySelector('head');
-        const script = document.createElement('script');
-        script.setAttribute(
-            'src',
-            'https://assets.calendly.com/assets/external/widget.js',
-        );
-        head!.appendChild(script);
-    }, []);
-
-    return (
-        <div className="flex h-[60vh] my-20 items-center justify-center">
-            <div
-                className="calendly-inline-widget"
-                data-url={url}
-                style={{ minHeight: '650px', width: '100%', height: '100%' }}
-            ></div>
-        </div>
+  useEffect(() => {
+    const head = document.querySelector('head');
+    const script = document.createElement('script');
+    script.setAttribute(
+      'src',
+      'https://assets.calendly.com/assets/external/widget.js',
     );
+    head!.appendChild(script);
+  }, []);
+
+  return (
+    <div className="my-20 flex h-[60vh] items-center justify-center">
+      <div
+        className="calendly-inline-widget"
+        data-url={url}
+        style={{ minHeight: '650px', width: '100%', height: '100%' }}
+      ></div>
+    </div>
+  );
 };
 
 export default CalendlyEmbed;
