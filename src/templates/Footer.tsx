194e5e46bb2a719badd0fdbbc00e9da417be39e0
Karim Fawaz <karimhilalfawaz@gmail.com>
2024-08-08 01:50:12 +0100

    Update Footer.tsx

--- src/templates/Footer.tsx
+++ src/templates/Footer.tsx
@@ -1,7 +1,6 @@
 import { Background } from '../background/Background';
 import { CenteredFooter } from '../footer/CenteredFooter';
 import { Section } from '../layout/Section';
-import { Logo } from './Logo';
 
 const Footer = () => (
   <Background color="bg-gray-100">
