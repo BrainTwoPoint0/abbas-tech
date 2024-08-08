c56a01c2cc29ab39d4083cdea056f8364ce90cb4
Karim Fawaz <karimhilalfawaz@gmail.com>
2024-08-08 01:43:15 +0100

    add office locations

--- src/templates/Footer.tsx
+++ src/templates/Footer.tsx
@@ -7,7 +7,10 @@ const Footer = () => (
   <Background color="bg-gray-100">
     <Section>
       <CenteredFooter logo={<Logo />} iconList={<></>}>
-        <></>
+        <div className="flex-col">
+          <h3>UK office: Abbas Technology Ltd</h3>
+          <h3>UAE office: Abbas Tech FZCO</h3>
+        </div>
       </CenteredFooter>
     </Section>
   </Background>
